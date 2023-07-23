1eea8fcbaa1d271c49f8aa1c57437de1f0d4fdf5
mimidoo-true <techqueenmimidoo@gmail>
2023-07-23 07:13:10 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,11 @@
-#include <stdio.h>
-  3 
-  4 /**
-  5  * main - Entry point
-  6  *
-  7  * Return: Always 0 (Success)
-  8  */
-  9 int main(void)
- 10 {
- 11         puts("\"Programming is like building a multilingual puzzle");
- 12         return (0);
- 13 }
+#include 3 #include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
